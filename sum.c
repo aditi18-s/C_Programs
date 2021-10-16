702cd9820d8eb5a80553427ed915a4e3bcba1daf
aditi18-s <76447342+aditi18-s@users.noreply.github.com>
2021-10-16 17:29:08 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int first_number, second_number;
+	int sum;
+	first_number=12;
+	second_number=38;
+	sum=first_number+second_number;
+	printf("the sum of two numbers is: %d",sum);
+	
+	return 0;
+	
+	}
